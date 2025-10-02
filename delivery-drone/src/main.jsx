7055133357f06279fb049c41582142e65b520a62
Yegon <yegonkimutai.99@gmail.com>
2025-10-02 10:55:15 +0300

    :children_crossing: Add routes and routers

--- delivery-drone/src/main.jsx
+++ delivery-drone/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import "./index.css";
+import Navigation from './components/Navigation';
+import Notifications from './components/Notifications';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+  <BrowserRouter>
+  <NavBar />
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/maps' element={<Navigation />} />
+    <Route path='/notifications' element={<Notifications />} />
+  </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
