9e0465967b4c65001c7f3e0b5f1f3e229283016e
Yegon <yegonkimutai.99@gmail.com>
2025-10-03 13:27:35 +0300

    :zap: Add content for maps

--- delivery-drone/src/components/Navigation.jsx
+++ delivery-drone/src/components/Navigation.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { MdMyLocation } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+import { IoNavigateOutline } from "react-icons/io5";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { IoMdTime } from "react-icons/io";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div>
+        <div>
+            <h1>Live Tracking</h1>
+            <button><MdMyLocation />My Location</button>
+        </div>
+        <div>
+            <input 
+                type="text"
+                placeholder="Search location or adress..."
+            />
+            <button>Search</button>
+        </div>
+        <div>
+            <button><IoLocationOutline /> Set Pickup</button>
+            <button><IoNavigateOutline /> Navigate</button>
+        </div>
+        <div>
+            <IoLocationOutline />
+            Interactive Map View
+        </div>
+        <div>
+        <h2><CiDeliveryTruck /> Active Deliveries</h2>
+        <div>
+        <div>
+            <p>Medical Supplies</p>
+            <p>Downtown District</p>
+        </div>
+        <div>
+            <p>in transit</p>
+            <p><IoMdTime />12 minutes</p>
+        </div>
+        </div>
+        <div>
+        <div>
+            <p>Groceries</p>
+            <p>Warehouse A</p>
+        </div>
+        <div>
+            <p>preparing</p>
+            <p><IoMdTime />25 minutes</p>
+        </div>
+        </div>
+        </div>
+    </div>
   )
 }
 
